65d17e89686e8188805c8aaf35a5d68a148d14b0
kais-candella <115159049+kais-candella@users.noreply.github.com>
2023-03-14 01:27:16 +0100

    Add files via upload

---
+++ Jour1/laplateforme.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM laplateforme.etudiants
+LIMIT 0, 1000
+
+-- Date: 2023-03-14 01:24
+*/
+INSERT INTO `` (`PersonID`,`Nom`,`Prenom`,`age`,`Email`) VALUES (1,'Candella','Kaïs',19,'kais.candella@laplateforme.io');
+INSERT INTO `` (`PersonID`,`Nom`,`Prenom`,`age`,`Email`) VALUES (2,'Spagetti','Bety',23,'betty.Spagetti@laplateforme.io');
+INSERT INTO `` (`PersonID`,`Nom`,`Prenom`,`age`,`Email`) VALUES (3,'Steak','Chuck',45,'Chuck.Steak@laplateforme.io');
+INSERT INTO `` (`PersonID`,`Nom`,`Prenom`,`age`,`Email`) VALUES (4,'Doe','Jhon',18,'Jhon.Doe@laplateforme.io');
+INSERT INTO `` (`PersonID`,`Nom`,`Prenom`,`age`,`Email`) VALUES (5,'barnes','binkie',16,'binkie.barnes@laplateforme.io');
+INSERT INTO `` (`PersonID`,`Nom`,`Prenom`,`age`,`Email`) VALUES (6,'dupuis','gertrude',20,'gertrude.dupuis@laplateforme.io');
